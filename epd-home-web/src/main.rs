aa75d6cba5ba6e4c9757012b92e2195f2d25c288
Jono Hill <jono@hillnz.com>
2024-04-15 21:21:40 +1200

    Support multiple stops, qoi

--- epd-home-web/src/main.rs
+++ epd-home-web/src/main.rs
@@ -1,27 +1,37 @@
 use std::{env, io::Cursor};
 
-use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder, ResponseError};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder, ResponseError};
 use epd_home::screen::{self, Screen};
+use image::{codecs::qoi::QoiEncoder, ImageEncoder};
 use serde::Deserialize;
 
 #[derive(thiserror::Error, Debug)]
 enum Error {
     #[error("Failed to create screen")]
-    ScreenError(#[from] screen::Error),
+    Screen(#[from] screen::Error),
+
+    #[error("Failed to decode image")]
+    Image(#[from] image::error::ImageError),
+
+    #[error("Failed to encode image")]
+    Bitmap(#[from] bmp_monochrome::BmpError),
 }
 
 impl ResponseError for Error {
     fn error_response(&self) -> HttpResponse {
         use Error::*;
+
+        log::error!("Error: {:?}", self);
         
         match self {
-            ScreenError(err) => {
+            Screen(err) => {
                 use screen::Error::*;
                 match err {
                     InvalidTimezone => HttpResponse::BadRequest().into(),
                     _ => HttpResponse::BadGateway().into(),
                 }
-            }
+            },
+            _ => HttpResponse::InternalServerError().into(),
         }
     }
 }
@@ -41,30 +51,72 @@ struct HomeScreenOptions {
     stop_code: String,
 }
 
-#[get("/home.bmp")]
-async fn get_home_screen(options: web::Query<HomeScreenOptions>) -> Result<impl Responder> {
-    let mut out_buffer = Cursor::new(Vec::<u8>::new());
+async fn render(options: web::Query<HomeScreenOptions>) -> Result<Vec<Vec<bool>>> {
+    let stop_codes: Vec<String> = options.stop_code.split(',').map(|s| s.to_string()).collect();
+    let stop_codes_ref = stop_codes.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
 
-    Screen::new(options.lat, options.lon, &options.timezone, &options.stop_code)?
-        .render(&mut out_buffer)
+    let img = Screen::new(options.lat, options.lon, &options.timezone, &stop_codes_ref)?
+        .render()
         .await?;
+
+    Ok(img)
+}
+
+#[get("/home.bmp")]
+async fn get_home_screen_bmp(options: web::Query<HomeScreenOptions>) -> Result<impl Responder> {
+    
+    let data = render(options).await?;
+
+    let mut buff = Cursor::new(Vec::new());
+    bmp_monochrome::Bmp::new(data)?.write(&mut buff)?;
 
     let response = HttpResponse::Ok()
         .content_type("image/bmp")
-        .body(out_buffer.into_inner());
+        .body(buff.into_inner());
+
+    Ok(response)
+}
+
+#[get("/home.qoi")]
+async fn get_home_screen_qoi(options: web::Query<HomeScreenOptions>) -> Result<impl Responder> {
+    
+    let img = render(options).await?;
+
+    let w = img[0].len() as u32;
+    let h = img.len() as u32;
+
+    let data: Vec<u8> = img.into_iter().flatten().flat_map(|px| {
+        if px {
+            [0, 0, 0]
+        } else {
+            [255, 255, 255]
+        }
+    })
+    .collect();
+
+    let mut buff = Cursor::new(Vec::new());
+    QoiEncoder::new(&mut buff)
+        .write_image(&data, w, h, image::ExtendedColorType::Rgb8)?;
+
+    let response = HttpResponse::Ok()
+        .content_type("image/qoi")
+        .body(buff.into_inner());
 
     Ok(response)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     let listen_address = env::var("LISTEN_ADDRESS").unwrap_or("127.0.0.1:8080".to_string());
 
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Compress::default())
             .service(ok)
-            .service(get_home_screen)
+            .service(get_home_screen_bmp)
+            .service(get_home_screen_qoi)
     })
     .bind(listen_address)?
     .run()
--- epd-home/src/screen.rs
+++ epd-home/src/screen.rs
@@ -2,7 +2,6 @@ use core::fmt;
 use std::{fmt::Display, io::Write, path::Path, sync::Arc};
 
 use askama::Template;
-use bmp_monochrome::Bmp;
 use chrono::{DateTime, NaiveDateTime, TimeZone, Timelike, Utc};
 use chrono_tz::Tz;
 use fontdb::Source;
@@ -84,7 +83,7 @@ fn icon_for_weather(code: u32, is_night: bool, gust_speed: f64) -> Icon {
 struct WeatherData {
     time: DateTime<Tz>,
     weather: Icon,
-    temp: Option<i64>,
+    temp: Option<String>,
 }
 
 #[derive(Debug)]
@@ -132,14 +131,14 @@ mod filters {
 #[template(path = "home.svg")]
 struct HomeSvgTemplate {
     weather_now: Icon,
-    temp_now: i64,
+    temp_now: String,
     time: DateTime<Tz>,
     forecast: Vec<WeatherData>,
     arrivals: Vec<ArrivalData>,
 }
 
 
-fn save_to_bmp<T: Write>(pixmap: Pixmap, output_file: &mut T) {
+fn save_to_image_bytes(pixmap: Pixmap) -> Vec<Vec<bool>> {
     // https://gitlab.com/efronlicht/dither/-/blob/master/src/bin/dither.rs?ref_type=heads
 
     let width = pixmap.width();
@@ -165,8 +164,8 @@ fn save_to_bmp<T: Write>(pixmap: Pixmap, output_file: &mut T) {
                 .collect_vec()
         })
         .collect_vec();
-    let bmp = Bmp::new(data).unwrap();
-    bmp.write(output_file).unwrap();
+    
+    data
 }
 
 fn load_icon(icon_name: &str) -> Option<Vec<u8>> {
@@ -180,12 +179,13 @@ fn load_icon(icon_name: &str) -> Option<Vec<u8>> {
         "sun.svg" => Some(include_bytes!("../assets/icons/sun.svg").to_vec()),
         "sunrise.svg" => Some(include_bytes!("../assets/icons/sunrise.svg").to_vec()),
         "sunset.svg" => Some(include_bytes!("../assets/icons/sunset.svg").to_vec()),
+        "wifi-off.svg" => Some(include_bytes!("../assets/icons/wifi-off.svg").to_vec()),
         "wind.svg" => Some(include_bytes!("../assets/icons/wind.svg").to_vec()),
         _ => None,
     }
 }
 
-async fn render_svg<T: Write>(svg_data: Vec<u8>, bmp_file: &mut T) {
+async fn render_svg(svg_data: Vec<u8>) -> Vec<Vec<bool>> {
     // Based on https://github.com/RazrFalcon/resvg/blob/master/crates/resvg/examples/minimal.rs
 
     log::debug!("Make SVG tree");
@@ -222,7 +222,7 @@ async fn render_svg<T: Write>(svg_data: Vec<u8>, bmp_file: &mut T) {
     pixmap.fill(Color::WHITE);
     resvg::render(&tree, tiny_skia::Transform::default(), &mut pixmap.as_mut());
 
-    save_to_bmp(pixmap, bmp_file);
+    save_to_image_bytes(pixmap)
 }
 
 fn parse_weather_time(time: &str, tz: &Tz) -> Result<DateTime<Tz>, chrono::ParseError> {
@@ -235,18 +235,18 @@ pub struct Screen {
     latitude: f64,
     longitude: f64,
     timezone: Tz,
-    stop_code: String,
+    stop_codes: Vec<String>,
 }
 
 impl Screen {
-    pub fn new(latitude: f64, longitude: f64, timezone: &str, stop_code: &str) -> Result<Self> {
+    pub fn new(latitude: f64, longitude: f64, timezone: &str, stop_codes: &[&str]) -> Result<Self> {
         let tz = timezone.parse().map_err(|_| Error::InvalidTimezone)?;
 
         let screen = Self {
             latitude,
             longitude,
             timezone: tz,
-            stop_code: stop_code.into(),
+            stop_codes: stop_codes.iter().map(|s| s.to_string()).collect(),
         };
         Ok(screen)
     }
@@ -267,7 +267,7 @@ impl Screen {
                 !weather.current.is_day,
                 weather.current.wind_gusts,
             ),
-            temp: Some(weather.current.temperature.round() as i64),
+            temp: Some(weather.current.temperature.round().to_string()),
         };
 
         let hourly_data = weather
@@ -277,7 +277,7 @@ impl Screen {
                 let d = WeatherData {
                     time: parse_weather_time(&data.time, &self.timezone)?,
                     weather: icon_for_weather(data.weather_code, !data.is_day, data.wind_gusts),
-                    temp: Some(data.temperature.round() as i64),
+                    temp: Some(data.temperature.round().to_string()),
                 };
                 Ok::<_, Error>(d)
             })
@@ -349,83 +349,84 @@ impl Screen {
     }
 
     async fn gather_arrivals(&self) -> Result<Vec<ArrivalData>> {
-        let arrivals = get_stop_arrivals(&self.stop_code).await?;
+        let pending_arrivals = self.stop_codes.iter().map(|code| get_stop_arrivals(code)).collect_vec();
+        let arrivals: Vec<_> = futures::future::join_all(pending_arrivals).await.into_iter()
+            .filter_map(|item| item.transpose())
+            .try_collect()?;
+        let arrivals = arrivals.into_iter().flatten().collect_vec();
+
         let now = Utc::now().with_timezone(&self.timezone);
 
-        if let Some(arrivals) = arrivals {
-            let data: Vec<_> = arrivals
-                .into_iter()
-                .filter(|arr| !arr.arrivals.is_empty())
-                .map(|arr| {
-                    let data = ArrivalData {
-                        route: arr.route_trip.route_short_name,
-                        headsign: arr.route_trip.stop_headsign,
-                        arrival_times: arr
-                            .arrivals
-                            .into_iter()
-                            .take(2)
-                            .map(|arr_time| {
-                                let time = arr_time
-                                    .updated_arrival_timestamp
-                                    .unwrap_or(arr_time.arrival_timestamp);
-                                let dt = Utc
-                                    .timestamp_millis_opt(time)
-                                    .single()
-                                    .unwrap()
-                                    .with_timezone(&self.timezone);
-                                let delta = dt - now;
+        let data: Vec<_> = arrivals
+            .into_iter()
+            .filter(|arr| !arr.arrivals.is_empty())
+            .map(|arr| {
+                let data = ArrivalData {
+                    route: arr.route_trip.route_short_name,
+                    headsign: arr.route_trip.stop_headsign,
+                    arrival_times: arr
+                        .arrivals
+                        .into_iter()
+                        .take(2)
+                        .map(|arr_time| {
+                            let time = arr_time
+                                .updated_arrival_timestamp
+                                .unwrap_or(arr_time.arrival_timestamp);
+                            let dt = Utc
+                                .timestamp_millis_opt(time)
+                                .single()
+                                .unwrap()
+                                .with_timezone(&self.timezone);
+                            let delta = dt - now;
 
-                                let arrival_time = match delta {
-                                    d if d.num_minutes() < 1 => ArrivalTime::Now,
-                                    d if d.num_minutes() < 100 => {
-                                        ArrivalTime::Minutes(d.num_minutes() as u32)
-                                    }
-                                    _ => ArrivalTime::Time(dt),
-                                };
-                                Ok(arrival_time)
-                            }) // only take a single time arrival
-                            .take_while_inclusive(|time| {
-                                matches!(time, Ok(ArrivalTime::Now) | Ok(ArrivalTime::Minutes(_)))
-                            })
-                            .collect::<Result<Vec<_>>>()?,
-                    };
-                    Ok::<_, Error>(data)
-                })
-                .try_collect()?;
+                            let arrival_time = match delta {
+                                d if d.num_minutes() < 1 => ArrivalTime::Now,
+                                d if d.num_minutes() < 100 => {
+                                    ArrivalTime::Minutes(d.num_minutes() as u32)
+                                }
+                                _ => ArrivalTime::Time(dt),
+                            };
+                            Ok(arrival_time)
+                        }) // only take a single time arrival
+                        .take_while_inclusive(|time| {
+                            matches!(time, Ok(ArrivalTime::Now) | Ok(ArrivalTime::Minutes(_)))
+                        })
+                        .collect::<Result<Vec<_>>>()?,
+                };
+                Ok::<_, Error>(data)
+            })
+            .try_collect()?;
 
-            let data = data
-                .into_iter()
-                // initial sort by arrival because we can only take 4
-                .map(|data| {
-                    (
-                        data.arrival_times
-                            .iter()
-                            .map(|time| time.minutes(&now))
-                            .min()
-                            .unwrap(),
-                        data,
-                    )
-                })
-                .sorted_by_key(|(time, _)| *time)
-                .take(4)
-                // Then attempt to order in a way to minimise redraws
-                // Everything an hour away or less first, then route, then headsign
-                .sorted_by_key(|(min_time, data)| {
-                    let time = *min_time > 60;
-                    let route = data.route.clone();
-                    let headsign = data.headsign.clone();
-                    (time, route, headsign)
-                })
-                .map(|(_, data)| data)
-                .collect::<Vec<_>>();
+        let data = data
+            .into_iter()
+            // initial sort by arrival because we can only take 4
+            .map(|data| {
+                (
+                    data.arrival_times
+                        .iter()
+                        .map(|time| time.minutes(&now))
+                        .min()
+                        .unwrap(),
+                    data,
+                )
+            })
+            .sorted_by_key(|(time, _)| *time)
+            .take(4)
+            // Then attempt to order in a way to minimise redraws
+            // Everything an hour away or less first, then route, then headsign
+            .sorted_by_key(|(min_time, data)| {
+                let time = *min_time > 60;
+                let route = data.route.clone();
+                let headsign = data.headsign.clone();
+                (time, route, headsign)
+            })
+            .map(|(_, data)| data)
+            .collect::<Vec<_>>();
 
-            Ok(data)
-        } else {
-            Ok(vec![])
-        }
+        Ok(data)
     }
 
-    pub async fn render<T: Write>(&self, to_bmp: &mut T) -> Result<()> {
+    pub async fn render(&self) -> Result<Vec<Vec<bool>>> {
         let (weather, transport) = join!(self.gather_weather(), self.gather_arrivals());
 
         let (current_weather, forecast) = weather?;
@@ -436,6 +437,7 @@ impl Screen {
             weather_now: current_weather.weather,
             temp_now: current_weather
                 .temp
+                .map(|temp| temp.to_string())
                 .ok_or_else(|| Error::MissingData("current temperature".into()))?,
             time: current_weather.time,
             forecast,
@@ -447,8 +449,48 @@ impl Screen {
 
         log::debug!("SVG data: {}", String::from_utf8_lossy(&svg_data));
 
-        render_svg(svg_data, to_bmp).await;
+        let img_data = render_svg(svg_data).await;
 
-        Ok(())
+        Ok(img_data)
     }
+
+    pub async fn render_placeholder<T: Write>(&self) -> Result<Vec<Vec<bool>>> {
+        let fake_now = Utc::now().with_timezone(&self.timezone).with_hour(12).unwrap().with_minute(0).unwrap();
+
+        let svg_data: Vec<u8> = HomeSvgTemplate {
+            weather_now: Icon::Cloud,
+            temp_now: "-".into(),
+            time: fake_now,
+            forecast: (1..=4).map(|n| {
+                WeatherData {
+                    time: fake_now.with_hour(n * 2 + 12).unwrap(),
+                    weather: Icon::Cloud,
+                    temp: Some("-".into())
+                }
+            }).collect_vec(),
+            arrivals: (1..=4).map(|n| {
+                ArrivalData {
+                    route: "---".into(),
+                    headsign: "----------".into(),
+                    arrival_times: vec![ArrivalTime::Minutes(n * 10)]
+                }
+            }).collect_vec()
+        }
+        .render()
+        .unwrap()
+        .into();
+
+        let data = render_svg(svg_data).await;
+
+        Ok(data)
+    }
+
+    pub async fn render_error<T: Write>(&self) -> Result<Vec<Vec<bool>>> {
+        let svg_data: Vec<u8> = include_bytes!("../assets/error.svg").into();
+
+        let data = render_svg(svg_data).await;
+
+        Ok(data)
+    }
+
 }
